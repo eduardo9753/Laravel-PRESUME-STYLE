fd494400a24424ad44a14ef159607ca2772fbefa
Eduardo <picis_9716@hotmail.com>
2025-04-02 21:45:51 -0500

    ECOMMERCE-PRESUME-STYLE

--- app/Http/Controllers/admin/role/RoleController.php
+++ app/Http/Controllers/admin/role/RoleController.php
@@ -14,7 +14,7 @@ class RoleController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
-        $this->middleware('can:lista roles');
+        //$this->middleware('can:lista roles');
     }
 
     public function index()
